e64de2169434cf15041568160aba305929468b65
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-08-09 22:41:35 -0300

    atualizacao effects scroll bar

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.html",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
